b5461c1120c383e1a2387284817a4155f457b716
Chlorine355 <akimoff.an@gmail.com>
2024-08-30 04:55:05 +0300

    characters cards basic
    to do: pretty cards; filter; modal; redesign everything for mobile too

--- src/pages/Characters.js
+++ src/pages/Characters.js
@@ -1,8 +1,10 @@
 import { useState } from 'react'
-
+import male_icon from '../icon-male.png'
+import female_icon from '../icon-female.png'
 
 const Characters = () => {
   const [characters, setCharacters] = useState([]);
+  // const [wookieCharacters, setWookieCharacters] = useState([]);
   const [loading, setLoading] = useState(false);
   const [next, setNext] = useState(1);
   const [totalCount, setTotalCount] = useState(0);
@@ -16,21 +18,34 @@ const Characters = () => {
         console.log(data);
         setCharacters( [...characters, ...data["results"]] );
         if (data["next"]) {setNext(next + 1)} else {setNext(-1)};
+        setTotalCount(data["count"]);
         setLoading(false);
       })
     }
   }
 
+  if (next == 1 && !loading) {
+    loadNextPage();
+  }
+
 
   return (
-    <>
-      <h1 onClick={loadNextPage}>Characters</h1>
-      <ul>
+    <div className="characters-wrapper">
+      <h1 style={{color: "white"}}>Pick anyone from {totalCount} characters!</h1>
+      <ul className="cards">
       { characters.map(character => {
-        return <li key={character.name}> {character.name} </li>
+        return  <li className="card" key={character.name}>
+            <span>{character.name}</span>
+            <span>Height: {character.height}</span>
+            <span>Mass: {character.mass}</span>
+            <span>Born: {character.birth_year}</span>
+            <span>Gender: {character.gender}</span>
+          </li>
       }) }
       </ul>
-    </>);
+      {next > 0 ? <div className="button yellow" onClick={loadNextPage}>Load more{loading ? "..." : ""}</div> : "" }
+      </div>
+    );
 };
 
 export default Characters;
