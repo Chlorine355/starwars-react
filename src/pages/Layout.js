583f4e495a74371f62d2ab476f0883252b2f6bd4
Chlorine355 <akimoff.an@gmail.com>
2024-08-31 02:32:28 +0300

    menu highlight

--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -1,18 +1,21 @@
 import { Outlet, Link } from "react-router-dom";
-import Logo from '../Logo'
+import Logo from '../Logo';
+import { useLocation } from 'react-router-dom';
+import { useEffect } from 'react';
 
 
 const Layout = () => {
+  let location = useLocation();
   return (
     <>
       <nav>
       <Logo/>
         <ul>
           <li>
-            <Link to="/">Home</Link>
+            <Link to="/" style={ {textShadow: location.pathname === "/" ? "0 0 15px white" : "none"} }>Home</Link>
           </li>
           <li>
-            <Link to="/Characters">Characters</Link>
+            <Link to="/Characters" style={ {textShadow: location.pathname === "/Characters" ? "0 0 15px white" : "none"}} >Characters</Link>
           </li>
         </ul>
       </nav>
